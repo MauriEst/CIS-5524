d99517ef1e28a2091c292f71ca150152c8533869
Mauricio Estrella <mauri_estrella@yahoo.com>
2025-01-29 00:16:00 -0500

    Add average path length and diameter calculations; add results output file

--- Assignment2/ComputeNetworkProperties.py
+++ Assignment2/ComputeNetworkProperties.py
@@ -23,17 +23,17 @@ for u, v in G_nx.edges():
     G_nk.addEdge(node_map[u], node_map[v])
 G_nk.indexEdges()
 
-# largest Connected Component Size
+# largest connected component size
 cc = nk.components.ConnectedComponents(G_nk)
 cc.run()
 largest_cc_size = max(cc.getComponentSizes().values())
 print(f"Largest Connected Component Size: {largest_cc_size}")
 
-# number of Connected Components
+# number of connected components
 num_components = cc.numberOfComponents()
 print(f"Number of Connected Components: {num_components}")
 
-# degree Distribution
+# degree distribution
 degrees = np.array([d for n, d in G_nx.degree()])
 avg_degrees = np.mean(degrees)
 degree_counts = np.bincount(degrees)
@@ -58,7 +58,21 @@ plt.grid(True)
 plt.savefig("degree_histogram.png")
 plt.show()
 
-# average path length or diameter
+# average path length and diameter
+sample_nodes = np.random.choice(list(node_map.values()), size=min(1000, G_nk.numberOfNodes()), replace=False)
+total_path_length = 0
+count = 0
+
+for node in sample_nodes:
+    sp = nk.distance.BFS(G_nk, node).run().getDistances()
+    valid_paths = [dist for dist in sp if dist > 0]
+    total_path_length += sum(valid_paths)
+    count += len(valid_paths)
+
+approx_avg_path_length = total_path_length / count
+print(f"Approximate Average Shortest Path Length (using 1000 random nodes): {approx_avg_path_length:.4f}")
+
+# diameter
 diameter_approx = nk.distance.Diameter(G_nk, algo=nk.distance.DiameterAlgo.EstimatedRange, error=0.1).run().getDiameter()
 print(f"Approximate Diameter: {diameter_approx}")
 print(f"Actual Diameter (longest shortest path): 44")
@@ -66,4 +80,3 @@ print(f"Actual Diameter (longest shortest path): 44")
 # average clustering coefficient - nx version
 avg_clustering_coeff = nx.average_clustering(G_nx)
 print(f"Average Clustering Coefficient: {avg_clustering_coeff:.4f}")
-
